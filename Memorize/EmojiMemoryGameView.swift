38ce7a76b08c4c3dcfc891772542e5152089f835
Treata <107116406+Treata11@users.noreply.github.com>
2022-09-22 15:50:20 +0330

    Alpha (#5)
    * a more readable code
    
    * Clean up Code
    
    * 2nd Task, add Theme.swift as a new Model, functional theme (VM & V)
    
    Co-authored-by: wempaus <58611349+wempaus@users.noreply.github.com>

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,23 +11,33 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var game: EmojiMemoryGame
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 66))]) {
-                ForEach(game .cards) { card in
-                    CardView(card)
-                        .aspectRatio(1000/1618, contentMode: .fit)
-                        .onTapGesture{
-                            game.choose(card)
+        VStack {
+            ScrollView {
+                Text(game.nameOfTheme).font(.largeTitle).foregroundColor(game.colorOfTheme).colorInvert()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 66))]) {
+                    ForEach(game.cards) { card in
+                        CardView(card)
+                            .aspectRatio(1000/1618, contentMode: .fit)
+                            .layoutPriority(100)
+                            .onTapGesture{
+                                game.choose(card)
+                            }
                         }
-                    } 
                 }
-            }
-            .foregroundColor(.orange ) //function with arguement label
-        .padding(.horizontal)
+                Button {
+                    game.newGame()
+                } label: {
+                    Image(systemName: "gamecontroller").font(.largeTitle)
+                }
+                }
+            .foregroundColor(game.colorOfTheme)
+            .padding(.horizontal)
+        }
     }
 
 struct CardView: View {
-    private let card: EmojiMemoryGame.Card
+    private let card: EmojiMemoryGame.Card  //MemoryGame<String>.Card
     
     init(_ card: EmojiMemoryGame.Card) {
         self.card = card
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -6,20 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     private var indexOfOnlyAndOnlyCardFaceUp: Int? {
         get { cards.indices.filter({ cards[$0].isFaceUp }).oneAndOnly }
-        set { cards.indices.forEach { cards[$0 ].isFaceUp = ($0 == newValue!) } }    //trailing closure syntax
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue!) } }
     }
     
-    mutating func choose(_ card: Card) -> Void {
+    internal mutating func choose(_ card: Card) -> Void {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
            !cards[chosenIndex].isFaceUp,
            !cards[chosenIndex].isMatched
         {
+            cards[chosenIndex].timesSeen += 1
+            
             if let potentialMatchIndex = indexOfOnlyAndOnlyCardFaceUp {
                 if cards[chosenIndex].content == cards[Int(potentialMatchIndex)].content {
                     cards[Int(potentialMatchIndex)].isMatched = true
@@ -32,7 +35,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
-    init(numberOfPairsOfCards: UInt8, creatCardContent: (UInt8) -> CardContent) {
+    init(numberOfPairsOfCards: Int, creatCardContent: (Int) -> CardContent) {
         cards = []
         
         for pairIndex in 0..<numberOfPairsOfCards {
@@ -42,32 +45,32 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
             cards.append(Card(content: content,
                 id: UInt8(pairIndex*2+1)))
             }
+        cards.shuffle()
         }
+    
+    //PropertyObserver
+//    var isFaceUp: Bool {
+//        willSet {
+//            if newValue {
+//                startUsingBonusTime()
+//            } else {
+//                stopUsingBonusTime()
+//            }
+//        }
+//    }
     
     struct Card: Identifiable {
         var isFaceUp = false
         var isMatched = false
+        var timesSeen: UInt8 = 0
         let content: CardContent
         let id: UInt8
     }
-    
-    // MARK: - Theme(s)
-    enum Theme: Equatable {
-        case Halloween(Emojis: Array<String>, numberOfPairsOfCards: UInt8, Color: String)
-        
-        case People(Emojis: [String], numberOfPairsOfCards: UInt8, Color: String)
-        
-        case Animals(Emojis: [String], numberOfPairsOfCards: UInt8, Color: String)
-        
-        case Flags(Emojis: [String], numberOfPairsOfCards: UInt8, Color: String)
-        
-        case Vehicles(Emojis: [String], numberOfPairsOfCards: UInt8, Color: String)
-        
-//        func chooseTheme(_ Theme: String) -> EmojiMemoryGame.Theme {
-//            return EmojiMemoryGame.Theme.People(Emojis: <#T##String#>, numberOfPairsOfCards: <#T##UInt8#>, Color: <#T##String#>) //bogus!
-    }
+
 }
-    
+
+// MARK: -Extention(s)
+
 extension Array {
     var oneAndOnly: Element?  {
         if self.count  == 1 {
@@ -78,17 +81,4 @@ extension Array {
     }
 }
 
-    //MARK: - Randomizer
-
-//    class LinearCongruentialGenerator  {
-//        var lastRandom = 42.0
-//        let m = 139968.0
-//        let a = 3877.0
-//        let c = 29573.0
-//        func random() -> Double {
-//            lastRandom = ((lastRandom * a + c)
-//                .truncatingRemainder(dividingBy:m))
-//            return lastRandom / m
-//        }
-//    }
 
