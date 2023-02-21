36144cf94bcca7cf856fd3dd7f226f1dcedc85dd
Treata11 <treata11@yahoo.com>
2023-02-21 17:33:26 +0330

    Fix model, Card, isFaceUP and isMatched with property observers to make the BonusTime actually work

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -75,8 +75,20 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
 //    }
     
     struct Card: Identifiable, Equatable {
-        var isFaceUp = false
-        var isMatched = false
+        var isFaceUp = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
         var hasAlreadyBeenSeen = false
         let content: CardContent
         let id: UInt8
