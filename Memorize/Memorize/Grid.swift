4a315ab40ff18e6c5ccad428f654909cc833a0bb
Treata11 <treata11@yahoo.com>
2023-03-22 00:38:35 +0330

    Create a ViewBuilder to with protocols and generics to grid items, in ourt case: Cards

---
+++ Memorize/Memorize/Grid.swift
@@ -0,0 +1,36 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Treata Norouzi on 3/21/23.
+//
+
+import SwiftUI
+
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
+    var items: [Item]
+    var viewForItem: (Item) -> ItemView
+    
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: size)
+        }
+    }
+    
+    func body(for item: Item, in size: CGSize) -> some View {
+        return viewForItem(item)
+    }
+}
+
+
