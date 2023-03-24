eb8703e7ec0448deb11d5323108c7fee8cd4cf88
Treata11 <treata11@yahoo.com>
2023-03-25 01:04:03 +0330

    Halfway through to A2 completion using enums to implement the concept of theme

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,13 +8,111 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createTheme(theme: themes.randomElement() ?? themes[0])
     
-    static func createMemoryGame() -> MemoryGame<String> {
-        let emojis: Array<String> = ["😃", "😍", "😝", "🧐", "😎", "😒", "😱", "☹️", "🥶", "😡", "😶‍🌫️", "🫥", "🤢", "💩", "🤔", "🤩", "🥳", "😜", "🤪", "😇", "🥹", "😂", "🥸", "🤯", "😳", "🫠", "😬", "😈", "🤡", "😻", "😿", "🤠", "😵‍💫"]
-        let randomNumberOfPairsOfCards = Int.random(in: 3...5)  /// (3...6).randomElement()!
-        return MemoryGame<String>(numberOfPairsOfCards: randomNumberOfPairsOfCards) { _ in
-            emojis.randomElement() ?? "😜🤪😵‍💫😳🫠🤡"
+//    static func createMemoryGame() -> MemoryGame<String> {
+//        let emojis: Array<String> = ["😃", "😍", "😝", "🧐", "😎", "😒", "😱", "☹️", "🥶", "😡", "😶‍🌫️", "🫥", "🤢", "💩", "🤔", "🤩", "🥳", "😜", "🤪", "😇", "🥹", "😂", "🥸", "🤯", "😳", "🫠", "😬", "😈", "🤡", "😻", "😿", "🤠", "😵‍💫"]
+//        let randomNumberOfPairsOfCards = Int.random(in: 3...5)  /// (3...6).randomElement()!
+//        return MemoryGame<String>(numberOfPairsOfCards: randomNumberOfPairsOfCards) { _ in
+//            emojis.randomElement() ?? "😜🤪😵‍💫😳🫠🤡"
+//        }
+//    }
+    
+    // MARK: - Theme
+    
+    enum Theme {
+        case animals(identifier: String, emojis: [String], numberOfPairsOfCards: Int, color: Color)
+        case sports(identifier: String, emojis: [String], numberOfPairsOfCards: Int, color: Color)
+        case faces(identifier: String, emojis: [String], numberOfPairsOfCards: Int, color: Color)
+    }
+    
+    static var themes: Array<Theme> = [
+        .animals(
+            identifier: "Animals",
+            emojis: [
+                "🐶", "🦊", "🐼", "🐯", "🦁", "🐸", "🐔", "🙈", "🐨", "🐌", "🦋", "🦄",
+                "🐣", "🐻‍❄️", "🐮", "🐰", "🐹", "🐞", "🐢", "🦖", "🐡", "🐬", "🦢", "🐿",
+                "🦔", "🕊", "🐈", "🦙", "🐏", "🐫", "🦣", "🦍", "🦈", "🦭"
+            ],
+            numberOfPairsOfCards: Int.random(in: 7...10),
+            color: .green
+        ),
+        .sports(
+            identifier: "Sports",
+            emojis: [
+                "⚽️", "🏀", "🏈", "⚾️", "🎾", "🏐", "🏉", "🎱", "🏓", "🏸", "🥊",
+                "🥋", "⛷", "⛹️‍♀️", "🏋️‍♀️", "🤼‍♀️", "🤽‍♂️", "🤾‍♂️", "🏌️‍♂️", "🏇", "🧘‍♂️", "🛹"
+            ],
+            numberOfPairsOfCards: Int.random(in: 6...8),
+            color: .blue
+        ),
+        .faces(
+            identifier: "Faces",
+            emojis: [
+                "😀", "😃", "😄", "😁", "😆", "😅", "🤣", "😂", "🙂", "🙃", "😉",
+                "😊", "😇", "🥰", "😍", "🤩", "😘", "😗", "😚", "😙", "😋", "😛",
+                "😜", "🤪", "😝", "🤑", "🤗", "🤭", "🤫", "🤔", "🤐", "🤨", "😐",
+                "😑", "😶", "🙄", "😏", "😒", "🙃", "🤥", "😬", "🤤", "😯", "😦",
+                "😧", "😮", "😲", "😴", "🤤", "🤢", "🤮", "🤧", "😵", "🤯", "🤠",
+                "😎", "🤓", "🧐", "😕", "😟", "🙁", "☹️", "😮", "😯", "😲", "😳",
+                "🥺", "😦", "😧", "😨", "😰", "😥", "😢", "😭", "😱", "😖", "😣",
+                "😞", "😓", "😩", "😫", "🥱", "😤", "😡", "😠", "🤬", "😈", "👿",
+                "💀", "☠️", "💩", "🤡", "👹", "👺", "👻", "👽", "👾", "🤖", "💩",
+                "👨‍🦰", "👩‍🦰", "👨‍🦳", "👩‍🦳", "👨‍🦲", "👩‍🦲", "🤮", "🤯", "🤪", "🤓", "🧐",
+                "🕵️‍♂️", "🧝‍♂️", "🤴", "👸", "🤶", "🦸‍♂️", "🦹‍♂️", "🧙‍♂️", "👨‍⚕️", "👩‍⚕️", "👨‍🎓",
+                "👩‍🎓", "👨‍⚖️", "👩‍⚖️", "👨‍🌾", "👩‍🌾", "👨‍🍳", "👩‍🍳", "👨‍🔧", "👩‍🔧", "👨‍🏭", "👩‍🏭",
+                "👨‍💼", "👩‍💼", "👨‍🔬", "👩‍🔬", "👨‍💻", "👩‍💻", "👨‍🎤", "👩‍🎤", "👨‍🎨", "👩‍🎨", "👨‍✈️",
+                "👩‍✈️", "👨‍🚀", "👩‍🚀", "👨‍🚒", "👩‍🚒", "👮‍♂️", "👮‍♀️", "🕵️‍♀️", "🧝‍♀️", "👱‍♂️", "👱‍♀️",
+                "🤰", "👼", "🤱", "👲", "🙍‍♂️", "🙍‍♀️", "🙎‍♂️", "🙎‍♀️", "🙅‍♂️", "🙅‍♀️", "🙆‍♂️",
+                "🙆‍♀️", "💁‍♂️", "💁‍♀️", "🙋‍♂️", "🙋‍♀️", "🙇‍♂️", "🙇‍♀️", "🤦‍♂️", "🤦‍♀️", "🤷‍♂️", "🤷‍♀️",
+                "💆‍♂️", "💆‍♀️", "💇‍♂️", "💇‍♀️", "🚶‍♂️", "🚶‍♀️", "🏃‍♂️", "🏃‍♀️", "💃", "🕺", "🤸‍♂️",
+                "🤸‍♀️", "🤼‍♂️", "🤼‍♀️", "🤽‍♂️", "🤽‍♀️", "🤾‍♂️", "🤾‍♀️", "🤹‍♂️", "🤹‍♀️", "👫", "👬",
+                "👭", "👨‍❤️‍👨", "👨‍❤️‍💋‍👨", "👩‍❤️‍👩", "👩‍❤️‍💋‍👩", "💑", "👨‍❤️‍👨", "👩‍❤️‍👩", "💏", "👩‍❤️‍💋‍👨", "👨‍❤️‍💋‍👩",
+                "👪", "👨‍👩‍👧", "👨‍👩‍👧‍👦", "👨‍👩‍👦‍👦",
+            ],
+            numberOfPairsOfCards: Int.random(in: 10...20),
+            color: .pink
+        ),
+    ]
+    
+//    static var themes: Array<Theme> {
+//        for theme in Theme.AllCases {
+//
+//        }
+//    }
+    
+//    var animalsEmojis: Theme = .animals(
+//        identifier: "Animals",
+//        emojis: [
+//            "🐶", "🦊", "🐼", "🐯", "🦁", "🐸", "🐔", "🙈", "🐨", "🐌", "🦋", "🦄",
+//            "🐣", "🐻‍❄️", "🐮", "🐰", "🐹", "🐞", "🐢", "🦖", "🐡", "🐬", "🦢", "🐿",
+//            "🦔", "🕊", "🐈", "🦙", "🐏", "🐫", "🦣", "🦍", "🦈", "🦭"
+//        ],
+//        numberOfPairsOfCards: Int.random(in: 6...16),
+//        color: .cream
+//    )
+    
+    
+    static func createTheme(theme: Theme) -> MemoryGame<String> {
+        switch theme {
+        case .animals(let identifier, let emojis, let numberOfPairsOfCards, let color):
+            let name = identifier
+            let color = color
+            return MemoryGame<String>(numberOfPairsOfCards: numberOfPairsOfCards) { _ in
+                emojis.randomElement() ?? "🦁🐯🐰🐴"
+            }
+        case .sports(let identifier, let emojis, let numberOfPairsOfCards, let color):
+            let name = identifier
+            let color = color
+            return MemoryGame<String>(numberOfPairsOfCards: numberOfPairsOfCards) { _ in
+                emojis.randomElement() ?? "⚽️🏀🏈⚾️"
+            }
+        case .faces(let identifier, let emojis, let numberOfPairsOfCards, let color):
+            let name = identifier
+            let color = color
+             return MemoryGame<String>(numberOfPairsOfCards: numberOfPairsOfCards) { _ in
+                emojis.randomElement() ?? "😃😍😝🧐"
+            }
         }
     }
     
@@ -25,8 +123,15 @@ class EmojiMemoryGame: ObservableObject {
     }
     
     // MARK: - Intent(s)
+    
     func choose(_ card: MemoryGame<String>.Card) {
         objectWillChange.send()
         model.choose(card)
     }
 }
+
+extension Color {
+    static let cream: Color = Color(red: 249/255, green: 223/255, blue: 205/255)
+}
+
+
