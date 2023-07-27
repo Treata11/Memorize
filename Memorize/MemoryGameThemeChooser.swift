a2dbb84fe2bfaaa9dd58880c0416cd729b4bdf69
Treata11 <treata11@yahoo.com>
2023-07-27 15:01:36 +0330

    Fix the issues with the List when editMode was active and fix the use of NavigationLink on changes of editMode and better Use of Bindings via getters and setters in View hierarchies.

--- Memorize/MemoryGameThemeChooser.swift
+++ Memorize/MemoryGameThemeChooser.swift
@@ -18,40 +18,27 @@ struct MemoryGameThemeChooser: View {
         NavigationView {
             List {
                 ForEach(emojiThemes.savedThemes) { theme in
-                    NavigationLink(destination: EmojiMemoryGameView(viewModel: EmojiMemoryGame(theme: theme))
-                        .navigationTitle(theme.name)) {
-                            ZStack(alignment: .leading) {
-                                VStack(alignment: .leading, spacing: 7) {
-                                    HStack() {
-                                        Text(theme.name).font(.title)
-                                        Spacer()
-                                        Text(cardCount(for: theme))
-                                    }
-                                    Text(emojiString(for: theme)).font(.largeTitle)
-                                }
-                                VStack(alignment: .trailing) {
-                                    if editMode.isEditing {
-                                        Button {
-                                            editCurrent(theme: theme)
-                                            print("success")
-                                        } label: {
-                                            Image(systemName: "square.and.pencil").font(.largeTitle)
-                                        }
-                                        .foregroundColor(Color(rgbaColor: theme.color)).colorInvert()
-                                    }
-                                }
-                            }
+                    if editMode.isEditing == true {
+                        NavigationLink(destination: ThemeEditor(theme: Binding(get: { theme }, set: { _ in return } ))) {
+                            ListOfThemes(theme: theme)
                         }
-                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
-                            Button(action: {
-                                editCurrent(theme: theme)
-                                print("Success")
+                        .listRowBackground(linearGradient(for: theme))
+                    } else {
+                        NavigationLink(destination: EmojiMemoryGameView(viewModel: EmojiMemoryGame(theme: theme))
+                            .navigationTitle(theme.name)) {
+                                ListOfThemes(theme: theme)
+                            }
+                            .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                                Button(action: {
+                                    editCurrent(theme: Binding(get: { theme }, set: { _ in return } ))
+                                    print("Success")
                                 }) {
                                     Label("Edit", systemImage: "pencil")
                                 }
                                 .tint(.blue)
-                        }
-                        .listRowBackground(color(for: theme))
+                            }
+                            .listRowBackground(linearGradient(for: theme))
+                    }
                 }
                 .onDelete { indexSet in
                     indexSet.map { emojiThemes.savedThemes[$0] }.forEach { theme in
@@ -84,20 +71,19 @@ struct MemoryGameThemeChooser: View {
         sheetPresented = true
     }
     
-    private func editCurrent(theme: Theme) {
-        selectedTheme = theme
+//    private func editCurrent(theme: Theme) {
+//        // Bug: The first time that this funciton is called
+//        //the state of the selectedTheme is not set to the theme
+//        selectedTheme = State(wrappedValue: theme).wrappedValue
+//        sheetPresented = true
+//    }
+    
+    private func editCurrent(theme: Binding<Theme>) {
+        selectedTheme = theme.wrappedValue as Theme
         sheetPresented = true
     }
     
-    private func emojiString(for theme: Theme) -> String {  // Bogus!
-        var emojis: Set<String> = []
-        for _ in 0..<5 {
-            emojis.insert(theme.emojis.randomElement()!)
-        }
-        return emojis.map{ String($0) }.joined(separator: " ")
-    }
-    
-    private func color(for theme: Theme) -> LinearGradient {
+    private func linearGradient(for theme: Theme) -> LinearGradient {
         LinearGradient(
             colors: [
                 Color(rgbaColor: theme.color).opacity(0.03),
@@ -106,6 +92,7 @@ struct MemoryGameThemeChooser: View {
                 Color(rgbaColor: theme.color).opacity(0.23),
                 Color(rgbaColor: theme.color).opacity(0.31),
                 Color(rgbaColor: theme.color).opacity(0.43),
+                Color(rgbaColor: theme.color).opacity(0.53),
                 Color(rgbaColor: theme.color).opacity(0.61),
                 Color(rgbaColor: theme.color).opacity(0.73),
                 Color(rgbaColor: theme.color).opacity(0.89),
@@ -115,13 +102,49 @@ struct MemoryGameThemeChooser: View {
             endPoint: .trailing
         )
     }
+ }
+
+struct ListOfThemes: View {
+    let theme: Theme
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 7) {
+                HStack() {
+                    Text(theme.name).font(.title)
+                    Spacer()
+                    Text(cardCount(for: theme))
+                }
+                Text(emojiString(for: theme)).font(.largeTitle)
+            }
+        }
+    }
+    
+    private func emojiString(for theme: Theme) -> String {  // Bogus!
+        let emojis = Array(theme.emojis)
+        let count = emojis.count - 1
+        let indices = [count/1, count/2, count/3, count/5]
+        
+        var result = [String]()
+        
+        for index in indices {
+            result.append(emojis[index])
+        }
+            
+        return result.map { String($0) }.joined(separator: " ")
+        //        var emojis: Set<String> = []
+        //        for _ in 0..<5 {
+        //            emojis.insert(theme.emojis.randomElement()!)
+        //        }
+        //        return emojis.map{ String($0) }.joined(separator: " ")
+    }
     
     private func cardCount(for theme: Theme) -> String {
         // TODO: Use CustomStringConvertible instead
         let description = "Pairs: "
         return description + String(theme.emojis.count)
     }
- }
+}
 
 struct MemoryGameThemeChooser_Previews: PreviewProvider {
     static var previews: some View {
--- Memorize/ThemeEditor.swift
+++ Memorize/ThemeEditor.swift
@@ -28,10 +28,9 @@ struct ThemeEditor: View {
     var body: some View {
         Form {
             Section("Name of the Theme") {
-                TextEditor(text: $themeToEdit.name)
+                TextEditor(text: $themeToEdit.name).unredacted()
             }
             Section("Manage Emojis") {
-//                Grid(emojiTheme.savedThemes.first!.emojis.map { String($0) }, id: \.self) { emoji in
                 AspectVGrid(items: themeToEdit.emojis.map { String($0) }, id: \.self, aspectRatio: 1) { emoji in
                     Text(emoji).font(Font.system(size: fontSize))
                         .onTapGesture {
@@ -73,52 +72,6 @@ struct ThemeEditor: View {
     
     var heightForRemovedEmojis: CGFloat {
         (CGFloat((themeToEdit.removedEmojis.count - 1) / 6) * 70 + 70 ) * 0.55
-    }
-    
-    let fontSize: CGFloat = 40
-}
-
-struct AbsoluteThemeEditor: View {
-    @EnvironmentObject var emojiTheme: EmojiThemes
-    
-//    @Binding var selectedTheme: Binding<Theme>
-    
-    var theme: Theme
-    
-//    init(selectedTheme: Binding<Theme>) {
-//        theme = selectedTheme.wrappedValue
-//        _selectedTheme = Binding(selectedTheme)
-//    }
-    
-    var body: some View {
-        Form {
-            Section("Name of the Theme") {
-                // MARK: Fix!
-//                TextEditor(text: $theme.name)
-            }
-            Section("Manage Emojis") {
-                AspectVGrid(items: theme.emojis.map { String($0) }, id: \.self, aspectRatio: 1) { emoji in
-                    Text(emoji).font(Font.system(size: fontSize))
-                        .onTapGesture {
-                            // TODO: the removing of the emojis
-                        }
-                }
-                .frame(height: height)
-            }
-            Section("Number of pairs of Cards") {
-                
-            }
-            Section("Choose Color") {
-                
-            }
-        }
-    }
-    
-    // MARK: - Drawing Constants
-
-    var height: CGFloat {
-        (CGFloat((theme.emojis.count - 1) / 6) * 70 + 70 ) * 0.55
-        // Paul played around and came up with this sort of calculation as best fit!
     }
     
     let fontSize: CGFloat = 40
